7224ed33f4eaf3540025e7cd286976994b029333
Gustavo Viana Calderaro Nogueira <gnogueira@seplan.intranet>
2024-01-17 11:24:52 -0300

    update

--- index.php
+++ index.php
@@ -7,10 +7,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/style.css">  
+    <link href="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/css/select2.min.css" rel="stylesheet" />    
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>  
+    <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.all.min.js"></script>    
     <script src="./js/enviar.js"></script>   
+    <script src="./js/pesquisar.js"></script>
     <title>Ficha de Iniciativa</title>
 </head>
 <body>
@@ -20,15 +23,15 @@
   <form class="form" id="form" action="post/Acao.php" method="POST">  
     <div id="sub-container">   
       <p class="texto">Eixo Estratégico</p>    
-      <select name="eixo_relacionado" required>
-      <option value="">todos</option>   
+      <select name="eixo_relacionado" class="select2" required>
+      <option value="">Selecione um eixo:</option>   
       <?php 
-        $exibicao1 = $conn->prepare('SELECT DISTINCT nome_eixo_estrategico from eixo');
+        $exibicao1 = $conn->prepare('SELECT DISTINCT cod_eixo_estrategico, nome_eixo_estrategico from eixo');
         $exibicao1->execute();
     
         if($exibicao1->rowCount() > 0){
           while($dado = $exibicao1->fetch(PDO::FETCH_ASSOC)){          
-            echo "<option value='{$dado['nome_eixo_estrategico']}'>{$dado['nome_eixo_estrategico']}</option>";          
+            echo "<option value='{$dado['cod_eixo_estrategico']} - {$dado['nome_eixo_estrategico']}'>{$dado['cod_eixo_estrategico']} - {$dado['nome_eixo_estrategico']}</option>";          
           }
         }  
       ?>
@@ -37,15 +40,16 @@
     </div> 
     <div id="sub-container">
       <p class="texto">Programa</p> 
-      <select name="problema_relacionado" required>
-        <option value="">todos</option>   
+      <select name="problema_relacionado" class="select2" required>
+        <option value="">Selecione um Programa:</option>   
         <?php 
-          $exibicao2 = $conn->prepare('SELECT DISTINCT desc_programa_governo from programa');
+          $exibicao2 = $conn->prepare('SELECT DISTINCT cod_programa_governo, desc_programa_governo from programa');
           $exibicao2->execute();
       
           if($exibicao2->rowCount() > 0){
             while($dado = $exibicao2->fetch(PDO::FETCH_ASSOC)){              
-              echo "<option value='{$dado['desc_programa_governo']}'>{$dado['desc_programa_governo']}</option>";         
+              echo "<option value='{$dado['cod_programa_governo']} - {$dado['desc_programa_governo']}'>
+              {$dado['cod_programa_governo']} - {$dado['desc_programa_governo']}</option>";         
             }
           }  
         ?>
@@ -54,15 +58,16 @@
     </div>
     <div id="sub-container">
       <p class="texto">Compromisso</p> 
-      <select name="compromisso_relacionado" required>
-      <option value="">todos</option> 
+      <select name="compromisso_relacionado" class="select2" required>
+      <option value="">Selecione um Compromisso:</option> 
       <?php 
-        $exibicao3 = $conn->prepare('SELECT DISTINCT desc_compromisso from compromisso');
+        $exibicao3 = $conn->prepare('SELECT DISTINCT cod_compromisso, desc_compromisso
+        from compromisso ORDER BY cod_compromisso');
         $exibicao3->execute();
     
         if($exibicao3->rowCount() > 0){
           while($dado = $exibicao3->fetch(PDO::FETCH_ASSOC)){         
-            echo "<option value='{$dado['desc_compromisso']}'>{$dado['desc_compromisso']}</option>";         
+            echo "<option value='{$dado['cod_compromisso']} - {$dado['desc_compromisso']}'>{$dado['cod_compromisso']} - {$dado['desc_compromisso']}</option>";         
           }
         }  
       ?>
