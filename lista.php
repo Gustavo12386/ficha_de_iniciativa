96089c70702aae9d3bc2f217da16ba41a8ac1dbe
Gustavo Viana Calderaro Nogueira <gnogueira@seplan.intranet>
2024-02-08 09:15:11 -0300

    update

--- lista.php
+++ lista.php
@@ -7,9 +7,11 @@ require 'post/Acoes.php';
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/lista.css">     
+    <link rel="stylesheet" href="./css/lista.css?v=4">     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+    <script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
     <title>Lista</title>
 </head>
 <body>
@@ -18,7 +20,11 @@ require 'post/Acoes.php';
     </div>  
     <div id="sub-container">           
     <input type="search" class="form-control w-25" placeholder="pesquisar por eixo" id="pesquisar">
-        <button onclick="searchData()" class="botao4">pesquisar</button>
+        <button onclick="searchData()" class="botao-pesquisa">pesquisar</button>
+    <input type="search" class="form-control w-25" placeholder="pesquisar por programa" id="pesquisar">
+        <button onclick="searchData()" class="botao-pesquisa">pesquisar</button> 
+    <input type="search" class="form-control w-25" placeholder="pesquisar por compromisso" id="pesquisar">
+        <button onclick="searchData()" class="botao-pesquisa">pesquisar</button>
     </div>
     <div id="tabela">    
         <table class="table table-bordered border-dark" border ='1'>
@@ -56,7 +62,7 @@ require 'post/Acoes.php';
         ?>
        </tbody>
        </table>
-       <input type="button" value="Voltar" class="botao3" onClick="window.location.href='index.php'">
+       <input type="button" value="Voltar" class="botao" onClick="window.location.href='index.php'">
     </div>
 </body>
 </html>
